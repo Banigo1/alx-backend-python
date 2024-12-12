8a56c42238deac1d7396c663ab8b1a4a935042ab
Precious <>
2024-12-12 10:44:39 +0100

    Parameterize a unit test

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -1,5 +1,25 @@
 #!/usr/bin/env python3
+"""
+Task 1
 
+Familiarize yourself with the utils.access_nested_map function and understand its purpose. Play with it in the Python console to make sure you understand.
+
+In this task you will write the first unit test for utils.access_nested_map.
+
+Create a TestAccessNestedMap class that inherits from unittest.TestCase.
+
+Implement the TestAccessNestedMap.test_access_nested_map method to test that the method returns what it is supposed to.
+
+Decorate the method with @parameterized.expand to test the function for following inputs:
+
+nested_map={"a": 1}, path=("a",)
+nested_map={"a": {"b": 2}}, path=("a",)
+nested_map={"a": {"b": 2}}, path=("a", "b")
+For each of these inputs, test with assertEqual that the function returns the expected result.
+
+The body of the test method should not be longer than 2 lines.
+
+"""
 import unittest
 from parameterized import parameterized
 from utils import access_nested_map
@@ -17,9 +37,19 @@ class TestAccessNestedMap(unittest.TestCase):
 if __name__ == "__main__":
     unittest.main()
 
-import unittest
-from parameterized import parameterized
-from utils import access_nested_map
+
+
+
+"""
+Task 2
+
+Implement TestAccessNestedMap.test_access_nested_map_exception. Use the assertRaises context manager to test that a KeyError is raised for the following inputs (use @parameterized.expand):
+
+nested_map={}, path=("a",)
+nested_map={"a": 1}, path=("a", "b")
+Also make sure that the exception message is as expected.
+
+"""
 
 class TestAccessNestedMap(unittest.TestCase):
     """Test cases for the access_nested_map function."""
