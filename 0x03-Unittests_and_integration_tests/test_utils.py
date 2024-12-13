bf43fb8253fc5ce771fb6f8f6a586b4bd05edb3a
Precious <>
2024-12-13 15:25:30 +0100

    Unittest

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -111,19 +111,16 @@ import unittest
 from unittest.mock import patch
 from functools import wraps
 
-
 def memoize(func):
+    """A decorator that caches the results of a function call."""
     cache = {}
 
-    @wraps(func)
-    def wrapper(*args, **kwargs):
-        key = str(args) + str(kwargs)
-        if key not in cache:
-            cache[key] = func(*args, **kwargs)
-        return cache[key]
+    def wrapper(*args):
+        if args not in cache:
+            cache[args] = func(*args)
+        return cache[args]
 
     return wrapper
-
 
 class TestClass:
     def a_method(self):
@@ -133,19 +130,22 @@ class TestClass:
     def a_property(self):
         return self.a_method()
 
-
 class TestMemoize(unittest.TestCase):
-    def test_memoize(self):
-        with patch.object(TestClass, 'a_method') as mock_method:
-            test_instance = TestClass()
-
-            result1 = test_instance.a_property
-            result2 = test_instance.a_property
-
-            self.assertEqual(result1, 42)
-            self.assertEqual(result2, 42)
-            mock_method.assert_called_once()
-
+    
+    @patch.object(TestClass, 'a_method', return_value=42)
+    def test_memoize(self, mock_a_method):
+        obj = TestClass()
+        
+        # Call a_property twice
+        result1 = obj.a_property()
+        result2 = obj.a_property()
+        
+        # Check that the result is correct
+        self.assertEqual(result1, 42)
+        self.assertEqual(result2, 42)
+        
+        # Ensure a_method was only called once
+        mock_a_method.assert_called_once()
 
 if __name__ == '__main__':
     unittest.main()
