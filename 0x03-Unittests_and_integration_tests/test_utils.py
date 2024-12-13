9382d10061bfbaf7ab58ecbb88584f12749ca56f
Precious <>
2024-12-13 18:36:54 +0100

    ...

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -53,7 +53,8 @@ class TestAccessNestedMap(unittest.TestCase):
             ex (Exception): The expected exception type to be raised.
 
         Asserts:
-            A KeyError should be raised when accessing an invalid path in the nested map.
+            A KeyError should be raised when
+            accessing an invalid path in the nested map.
         """
         with self.assertRaises(ex):
             access_nested_map(map, path)
