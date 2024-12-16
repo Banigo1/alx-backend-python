a2d07495e8155371fb3dbb5ea0c0cb0ab9d218dc
Precious <>
2024-12-16 19:18:22 +0100

    ...

--- 0x03-Unittests_and_integration_tests/test_client.py
+++ 0x03-Unittests_and_integration_tests/test_client.py
@@ -21,7 +21,7 @@ from parameterized import parameterized_class
 from client import GithubOrgClient
 from fixtures import org_payload, repos_payload, expected_repos, apache2_repos
 
-#-------------------------------------------------Task 4
+# -------------------------------------------------Task 4
 
 
 class TestGithubOrgClient(unittest.TestCase):
@@ -34,7 +34,7 @@ class TestGithubOrgClient(unittest.TestCase):
     @patch('client.get_json')
     def test_org(self, input, mock):
         """ test that GithubOrgClient.org returns the correct value."""
-   test_class = GithubOrgClient(input)test_class.org()mock.called_with_once(test_class.ORG_URL)
+        test_class = GithubOrgClient(input)test_class.org()mock.called_with_once(test_class.ORG_URL)
 
 
     def test_public_repos_url(self):
