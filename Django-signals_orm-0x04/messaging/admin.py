a04174cee7ea6dc4e2ccdc4715c8c633756c7bba
Precious <banigoprecious@yahoo.com>
2025-01-25 12:11:53 +0100

    Django-signals_orm-0x04

--- Django-signals_orm-0x04/messaging/admin.py
+++ Django-signals_orm-0x04/messaging/admin.py
@@ -1,11 +1,3 @@
 from django.contrib import admin
 
-# Allow users to view previous versions of their messages in the admin interface.
-
-@admin.register(Message)
-class MessageAdmin(admin.ModelAdmin):
-    list_display = ('sender', 'receiver', 'timestamp', 'edited')
-
-@admin.register(MessageHistory)
-class MessageHistoryAdmin(admin.ModelAdmin):
-    list_display = ('message', 'old_content', 'edited_at')
+# Register your models here.
