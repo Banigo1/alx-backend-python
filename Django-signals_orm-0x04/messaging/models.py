6271ccce942dec1a0962dade1ab055cc47d0eea1
Precious <banigoprecious@yahoo.com>
2025-01-25 14:14:39 +0100

    ...

--- Django-signals_orm-0x04/messaging/models.py
+++ Django-signals_orm-0x04/messaging/models.py
@@ -1,17 +1,27 @@
 from django.db import models
 from django.contrib.auth.models import User
+from .managers import UnreadMessagesManager
 
 class Message(models.Model):
-    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='messages')
-    content = models.TextField()
-    timestamp = models.DateTimeField(auto_now_add=True)
+  sender = models.ForeignKey(User, on_delete=models.CASCADE, related_name='sender')
+  receiver = models.ForeignKey(User, on_delete=models.CASCADE, related_name='receiver')
+  content = models.TextField()
+  timestamp = models.DateTimeField(auto_now_add=True)
+  edited = models.BooleanField(default=False)
+  parent_message = models.ForeignKey('self', on_delete=models.CASCADE, null=True, blank=True, related_name='replies')
+  read = models.BooleanField(default=False)
+
+  objects = models.Manager()
+  unread = UnreadMessagesManager()
 
 class Notification(models.Model):
-    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='notifications')
-    message = models.TextField()
-    created_at = models.DateTimeField(auto_now_add=True)
+    user = models.ForeignKey(User, related_name="notifications", on_delete=models.CASCADE)
+    message = models.ForeignKey(Message, related_name="notifications", on_delete=models.CASCADE)
+    is_read = models.BooleanField(default=False)
+    timestamp = models.DateTimeField(auto_now_add=True)
 
 class MessageHistory(models.Model):
-    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='message_histories')
-    history = models.TextField()
-    timestamp = models.DateTimeField(auto_now_add=True)
+    message = models.ForeignKey(Message, on_delete=models.CASCADE, related_name='history')
+    old_content = models.TextField()
+    edited_at = models.DateTimeField(auto_now_add=True)
+    edited_by = models.ForeignKey(User, on_delete=models.CASCADE, null=True, related_name='edited_message')
