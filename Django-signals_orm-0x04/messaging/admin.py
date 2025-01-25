231ec1e17fb966894a3458f7638da9b9b9d41f9f
Precious <banigoprecious@yahoo.com>
2025-01-25 11:14:35 +0100

    1. Create a Signal for Logging Message Edits

--- Django-signals_orm-0x04/messaging/admin.py
+++ Django-signals_orm-0x04/messaging/admin.py
@@ -1,10 +1,12 @@
 from django.contrib import admin
-from .models import Message, Notification
+from .models import Message, MessageHistory
+
+# Allow users to view previous versions of their messages in the admin interface.
 
 @admin.register(Message)
 class MessageAdmin(admin.ModelAdmin):
-    list_display = ('sender', 'receiver', 'timestamp')
+    list_display = ('sender', 'receiver', 'timestamp', 'edited')
 
-@admin.register(Notification)
-class NotificationAdmin(admin.ModelAdmin):
-    list_display = ('user', 'message', 'created_at')
+@admin.register(MessageHistory)
+class MessageHistoryAdmin(admin.ModelAdmin):
+    list_display = ('message', 'old_content', 'edited_at')
