cf2d87af195008dddce3148b38788b6261c99cce
Precious <banigoprecious@yahoo.com>
2025-01-25 17:59:37 +0100

    ...

--- Django-signals_orm-0x04/messaging/signals.py
+++ Django-signals_orm-0x04/messaging/signals.py
@@ -2,6 +2,7 @@ from django.db.models.signals import post_delete
 from django.dispatch import receiver
 from django.contrib.auth.models import User
 from .models import Message, Notification, MessageHistory
+from django.db import models
 
 @receiver(post_delete, sender=User)
 def delete_related_data(sender, instance, **kwargs):
@@ -13,3 +14,8 @@ def delete_related_data(sender, instance, **kwargs):
 
     # Delete all message histories associated with the user
     MessageHistory.objects.filter(user=instance).delete()
+
+class MessageHistory(models.Model):
+    message = models.ForeignKey('Message', on_delete=models.CASCADE)
+    action = models.CharField(max_length=10)  # e.g., 'created', 'updated'
+    timestamp = models.DateTimeField(auto_now_add=True)
