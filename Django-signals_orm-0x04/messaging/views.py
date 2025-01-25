2e360a1809cc9a9fceb6ba85e2efab5ce237eedb
Precious <banigoprecious@yahoo.com>
2025-01-25 19:24:23 +0100

    ...

--- Django-signals_orm-0x04/messaging/views.py
+++ Django-signals_orm-0x04/messaging/views.py
@@ -1,7 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.views.decorators.cache import cache_page
 from .models import Message # the Message model
 from django.db.models import Prefetch
+from django.contrib.auth.decorators import login_required
 
 @cache_page(60)  # Cache the view for 60 seconds
 def conversation_messages(request, conversation_id):
@@ -28,3 +29,18 @@ def unread_messages(request):
 def message_list(request):
     messages = Message.objects.prefetch_related(Prefetch('replies')).all()
     return render(request, 'your_template.html', {'messages': messages})
+
+
+@login_required
+def send_message(request):
+    if request.method == 'POST':
+        content = request.POST.get('content')
+        receiver_id = request.POST.get('receiver_id')
+        receiver = User.objects.get(id=receiver_id)
+
+        # Create a new message
+        Message.objects.create(content=content, sender=request.user, receiver=receiver)
+
+        return redirect('message_list')  # Redirect to the message list or another appropriate view
+
+    return render(request, 'send_message.html')  # Render the message sending form
